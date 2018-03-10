c705a53805993af4f43956c40cbaa60cf7db830b
João Reis <joao.r.reis@outlook.com>
2018-03-10 18:25:10 +0000

    init sln

---
+++ src/TplExperiments/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TplExperiments
+{
+    using System;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
