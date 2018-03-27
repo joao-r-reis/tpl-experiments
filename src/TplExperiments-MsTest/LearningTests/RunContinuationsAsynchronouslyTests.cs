ae40af411990a16f9da818cffd4f848891486d4f
João Reis <joao.r.reis@outlook.com>
2018-03-27 22:11:23 +0100

    added learning tests for runcontinuationsasync

---
+++ src/TplExperiments-MsTest/LearningTests/RunContinuationsAsynchronouslyTests.cs
@@ -0,0 +1,61 @@
+namespace TplExperiments_MsTest
+{
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public async Task Tcs_RunContinuationsAsync_ContinuationsExecutedOnSingleThread()
+        {
+            var threadIds = new ConcurrentBag<int>();
+            var tcs = new TaskCompletionSource<bool>();
+            var tl = new List<Task>();
+            for (var i = 0L; i < 100000L; i++)
+            {
+                tl.Add(this.AwaitTask(threadIds, tcs.Task));
+            }
+            
+            await Task.Delay(1000);
+            tcs.TrySetResult(true);
+
+            await Task.WhenAll(tl);
+            Assert.AreEqual(1, threadIds.Distinct().Count());
+        }
+
+        [TestMethod]
+        public async Task Tcs_RunContinuationsAsync_ContinuationsExecutedOnMultipleThreads()
+        {
+            var threadIds = new ConcurrentBag<int>();
+            var tcs = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+            var tl = new List<Task>();
+            for (var i = 0L; i < 100000L; i++)
+            {
+                tl.Add(this.AwaitTask(threadIds, tcs.Task));
+            }
+            
+            await Task.Delay(1000);
+            tcs.TrySetResult(true);
+
+            await Task.WhenAll(tl);
+            Assert.AreNotEqual(1, threadIds.Distinct().Count());
+        }
+
+        private async Task AwaitTask(ConcurrentBag<int> strings, Task t)
+        {
+            await t;
+            strings.Add(System.Environment.CurrentManagedThreadId);
+        }
+
+        // TODO:
+        // https://blogs.msdn.microsoft.com/pfxteam/2011/10/24/task-run-vs-task-factory-startnew/
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
