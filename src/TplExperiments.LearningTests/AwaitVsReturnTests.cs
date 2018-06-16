702d1a8bd345405bd289e06830daca4d934a4fa7
João Reis <joao.r.reis@outlook.com>
2018-06-16 23:24:34 +0100

    added await vs return tests

---
+++ src/TplExperiments.LearningTests/AwaitVsReturnTests.cs
@@ -0,0 +1,53 @@
+﻿namespace TplExperiments.LearningTests
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class AwaitVsReturnTests
+    {
+        [TestMethod]
+        public void MethodAwaitsTask_CallWithoutAwait_DoesntThrowException()
+        {
+            // Arrange
+            // Act
+            var task = InvalidDelayAwaitTask();
+
+            // Assert
+            Assert.IsTrue(task.IsFaulted);
+        }
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void MethodReturnsTask_CallWithoutAwait_ThrowsException()
+        {
+            // Arrange
+            // Act
+            // Assert
+            var task = InvalidDelayReturnTask();
+        }
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public async Task MethodAwaitsTask_CallWithAwait_ThrowsException()
+        {
+            // Arrange
+            // Act
+            var task = InvalidDelayAwaitTask();
+
+            // Assert
+            await task;
+        }
+
+        static async Task InvalidDelayAwaitTask()
+        {
+            await Task.Delay(-5);
+        }
+
+        static Task InvalidDelayReturnTask()
+        {
+            return Task.Delay(-5);
+        }
+    }
+}
